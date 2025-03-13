83afbab991d4096be635d3e1801c0e1c3c7f6ad8
hoang-bh-1203 <bhhoang1203@gmail.com>
2025-03-14 00:20:26 +0700

    Config theme MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import { Button } from '@mui/material'
+import { Box, FormControl, InputLabel, MenuItem } from '@mui/material'
+import {
+  useColorScheme
+} from '@mui/material/styles'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="demo-select-small"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 
 function App() {
 
   return (
     <>
-      <div>Bui Hoang</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <ModeSelect />
     </>
   )
 }
