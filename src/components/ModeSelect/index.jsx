22c59253740972e4eb990fef9fff98a1d6941347
phamduc29012003 <bhhoang1203@gmail.com>
2025-03-17 22:50:08 +0700

    feat: adding AppBar UI

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -12,7 +12,7 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+    <FormControl size="small" sx={{ minWidth: 120 }}>
       <InputLabel id="label-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="label-dark-light-mode"
